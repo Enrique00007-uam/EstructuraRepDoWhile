a245c28f0bc025d1aded91c843ecaa153d9fd8f6
Fra619-ghost <fxaaguilera@uamv.edu.ni>
2024-05-24 08:40:27 -0600

    Se agrega el dev6

---
+++ dev6.cpp/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "proceso.cpp"
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    inicializar();
+    return 0;
+}
---
+++ dev6.cpp/variables.h
@@ -0,0 +1,5 @@
+
+const int MAX_NUMBERS = 100; // Definimos un tamaño máximo para el array
+int numbers[MAX_NUMBERS];
+int posicion = 0;
+int suma = 0;
